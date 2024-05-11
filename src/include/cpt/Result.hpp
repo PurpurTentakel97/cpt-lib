c2689119ffd4029260446285dcd3b77069be90a3
Martin <martin.schreurs@netcologne.de>
2024-05-12 00:39:34 +0200

    result

--- src/include/cpt/Result.hpp
+++ src/include/cpt/Result.hpp
@@ -5,21 +5,35 @@
 //
 
 #pragma once
+#include <functional>
 #include <stdexcept>
 #include <variant>
 
 namespace cpt {
+    template<typename E>
+    struct Err final {
+    private:
+        E m_value;
+
+    public:
+        explicit Err(E value) : m_value{ std::move(value) } { }
+
+        [[nodiscard]] E value() const {
+            return m_value;
+        }
+    };
+
     template<typename T, typename E>
     class Result final {
     private:
         std::variant<T, E> m_value;
 
     public:
-        explicit Result(T value) : m_value{ std::move(value) } { }
-        explicit Result(E value) : m_value{ std::move(value) } { }
+        Result(T value) : m_value{ std::in_place_index<0>, std::forward<T>(value) } { }
+        Result(Err<E> value) : m_value{ std::in_place_index<1>, std::forward<E>(value.value()) } { }
 
         [[nodiscard]] bool ok() const {
-            return std::holds_alternative<T>(m_value);
+            return m_value.index() == 0;
         }
 
         [[nodiscard]] bool err() const {
@@ -28,20 +42,28 @@ namespace cpt {
 
         [[nodiscard]] T unwrap() const {
             if (ok()) {
-                return std::get<T>(m_value);
+                return std::get<0>(m_value);
             }
             throw std::logic_error("while unwrapping a result with 'unwarp'");
         }
 
         [[nodiscard]] E unwrap_err() const {
             if (err()) {
-                return std::get<E>(m_value);
+                return std::get<1>(m_value);
             }
             throw std::logic_error("while unwrapping an result with 'unwrap_err'");
         }
 
         [[nodiscard]] T unwrap_or(T const& other) const {
             return ok() ? unwrap() : other;
+        }
+
+        [[nodiscard]] T unwarp_or_else(std::function<T()> const& func) const {
+            return ok() ? unwrap() : func();
+        }
+
+        [[nodiscard]] T unwrap_or_default() const {
+            return ok() ? unwrap() : T();
         }
     };
 } // namespace cpt
--- test/Test_Result.cpp
+++ test/Test_Result.cpp
@@ -9,15 +9,31 @@
 
 enum class Error {
     default_error,
+    other_error,
 };
 
-enum class Result {
-    default_result,
-    other_result,
+class Value {
+private:
+    int m_value;
+
+public:
+    explicit Value(int value) : m_value{ value } { }
+    Value() = default;
+
+    [[nodiscard]] int value() const {
+        return m_value;
+    }
+
+    friend bool operator==(Value const& lhs, Value const& rhs);
 };
 
-TEST(Result, Construct_Result) {
-    auto const result = cpt::Result<Result, Error>(Result::default_result);
+bool operator==(Value const& lhs, Value const& rhs) {
+    return lhs.m_value == rhs.m_value;
+}
+
+
+TEST(Result, Construct_OK) {
+    auto const result = cpt::Result<Value, Error>(Value(1));
 
     auto const test_exception = [result]() {
         try {
@@ -30,13 +46,15 @@ TEST(Result, Construct_Result) {
 
     ASSERT_TRUE(result.ok());
     ASSERT_FALSE(result.err());
-    ASSERT_EQ(result.unwrap(), Result::default_result);
+    ASSERT_EQ(result.unwrap(), Value(1));
     ASSERT_THROW(test_exception(), std::logic_error);
-    ASSERT_EQ(result.unwrap_or(Result::other_result), Result::default_result);
+    ASSERT_EQ(result.unwrap_or(Value(2)), Value(1));
+    ASSERT_EQ(result.unwarp_or_else([]() { return Value(2); }), Value(1));
+    ASSERT_EQ(result.unwrap_or_default(), Value(1));
 }
 
 TEST(Result, Construct_Error) {
-    auto const result = cpt::Result<Result, Error>(Error::default_error);
+    auto const result = cpt::Result<Value, Error>(cpt::Err(Error::default_error));
 
     auto const test_exception = [result]() {
         try {
@@ -51,5 +69,48 @@ TEST(Result, Construct_Error) {
     ASSERT_FALSE(result.ok());
     ASSERT_EQ(result.unwrap_err(), Error::default_error);
     ASSERT_THROW(test_exception(), std::logic_error);
-    ASSERT_EQ(result.unwrap_or(Result::default_result), Result::default_result);
+    ASSERT_EQ(result.unwrap_or(Value(2)), Value(2));
+    ASSERT_EQ(result.unwarp_or_else([]() { return Value(2); }), Value(2));
+    ASSERT_EQ(result.unwrap_or_default(), Value(0));
+}
+
+TEST(Result, Construct_OK_Same_type) {
+    auto const result         = cpt::Result<Value, Value>(Value(1));
+    auto const test_exception = [result]() {
+        try {
+            [[maybe_unused]] auto const r = result.unwrap_err();
+        } catch (std::logic_error const& e) {
+            EXPECT_STREQ(e.what(), "while unwrapping an result with 'unwrap_err'");
+            throw;
+        }
+    };
+
+    ASSERT_TRUE(result.ok());
+    ASSERT_FALSE(result.err());
+    ASSERT_EQ(result.unwrap(), Value(1));
+    ASSERT_THROW(test_exception(), std::logic_error);
+    ASSERT_EQ(result.unwrap_or(Value(2)), Value(1));
+    ASSERT_EQ(result.unwarp_or_else([]() { return Value(2); }), Value(1));
+    ASSERT_EQ(result.unwrap_or_default(), Value(1));
+}
+
+TEST(Result, Construct_Error_Same_type) {
+    auto const result = cpt::Result<Error, Error>(cpt::Err(Error::default_error));
+
+    auto const test_exception = [result]() {
+        try {
+            [[maybe_unused]] auto const r = result.unwrap();
+        } catch (std::logic_error const& e) {
+            EXPECT_STREQ(e.what(), "while unwrapping a result with 'unwarp'");
+            throw;
+        }
+    };
+
+    ASSERT_TRUE(result.err());
+    ASSERT_FALSE(result.ok());
+    ASSERT_EQ(result.unwrap_err(), Error::default_error);
+    ASSERT_THROW(test_exception(), std::logic_error);
+    ASSERT_EQ(result.unwrap_or(Error::other_error), Error::other_error);
+    ASSERT_EQ(result.unwarp_or_else([](){return Error::other_error;}), Error::other_error);
+    ASSERT_EQ(result.unwrap_or_default(), Error::default_error);
 }
