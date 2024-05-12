bd66f45546a1fff587a0a291443971b15655ea00
Martin <martin.schreurs@netcologne.de>
2024-05-12 16:34:36 +0200

    make everything constexpr

--- src/include/cpt/Optional.hpp
+++ src/include/cpt/Optional.hpp
@@ -16,21 +16,21 @@ namespace cpt {
         std::variant<T, std::monostate> m_value;
 
     public:
-        Optional(T&& value) : m_value{ std::in_place_index<0>, std::forward<T>(value) } { }
-        Optional() : m_value{ std::in_place_index<1>, std::monostate() } { }
+        constexpr Optional(T&& value) : m_value{ std::in_place_index<0>, std::forward<T>(value) } { }
+        constexpr Optional() : m_value{ std::in_place_index<1>, std::monostate() } { }
 
-        [[nodiscard]] bool ok() const {
+        [[nodiscard]] constexpr bool ok() const {
             return m_value.index() == 0;
         }
 
-        [[nodiscard]] T& unwrap() {
+        [[nodiscard]] constexpr T& unwrap() {
             if (ok()) {
                 return std::get<0>(m_value);
             }
             throw std::logic_error("while unwrapping an optional");
         }
 
-        [[nodiscard]] T const& unwrap() const {
+        [[nodiscard]] constexpr T const& unwrap() const {
             if (ok()) {
                 return std::get<0>(m_value);
             }
@@ -38,11 +38,11 @@ namespace cpt {
         }
 
         template<typename  F> requires std::invocable<F>
-        [[nodiscard]] T unwrap_or_else(F const& func) const {
+        [[nodiscard]] constexpr T unwrap_or_else(F const& func) const {
             return ok() ? unwrap() : func();
         }
 
-        [[nodiscard]] T unwrap_or_default() const requires(std::default_initializable<T>) {
+        [[nodiscard]] constexpr T unwrap_or_default() const requires(std::default_initializable<T>) {
             return ok() ? unwrap() : T();
         }
     };
--- src/include/cpt/Result.hpp
+++ src/include/cpt/Result.hpp
@@ -16,9 +16,9 @@ namespace cpt {
         E m_value;
 
     public:
-        explicit Err(E value) : m_value{ std::move(value) } { }
+        explicit constexpr Err(E value) : m_value{ std::move(value) } { }
 
-        [[nodiscard]] E value() const {
+        [[nodiscard]] constexpr E value() const {
             return m_value;
         }
     };
@@ -29,55 +29,58 @@ namespace cpt {
         std::variant<T, E> m_value;
 
     public:
-        Result(T&& value) : m_value{ std::in_place_index<0>, std::forward<T>(value) } { }
-        Result(Err<E>&& value) : m_value{ std::in_place_index<1>, std::forward<E>(value.value()) } { }
+        constexpr Result(T&& value) : m_value{ std::in_place_index<0>, std::forward<T>(value) } { }
+        constexpr Result(Err<E>&& value) : m_value{ std::in_place_index<1>, std::forward<E>(value.value()) } { }
 
-        [[nodiscard]] bool ok() const {
+        [[nodiscard]] constexpr bool ok() const {
             return m_value.index() == 0;
         }
 
-        [[nodiscard]] bool err() const {
+        [[nodiscard]] constexpr bool err() const {
             return not ok();
         }
 
-        [[nodiscard]] T const& unwrap() const {
+        [[nodiscard]] constexpr T const& unwrap() const {
             if (ok()) {
                 return std::get<0>(m_value);
             }
             throw std::logic_error("while unwrapping a result with 'unwarp'");
         }
 
-        [[nodiscard]] T& unwrap() {
+        [[nodiscard]] constexpr T& unwrap() {
             if (ok()) {
                 return std::get<0>(m_value);
             }
             throw std::logic_error("while unwrapping a result with 'unwarp'");
         }
 
-        [[nodiscard]] E const& unwrap_err() const {
+        [[nodiscard]] constexpr  E const& unwrap_err() const {
             if (err()) {
                 return std::get<1>(m_value);
             }
             throw std::logic_error("while unwrapping an result with 'unwrap_err'");
         }
 
-        [[nodiscard]] E& unwrap_err() {
+        [[nodiscard]] constexpr E& unwrap_err() {
             if (err()) {
                 return std::get<1>(m_value);
             }
             throw std::logic_error("while unwrapping an result with 'unwrap_err'");
         }
 
-        [[nodiscard]] T unwrap_or(T const& other) const {
+        [[nodiscard]] constexpr T unwrap_or(T const& other) const {
             return ok() ? unwrap() : other;
         }
 
-        template <typename F> requires std::invocable<F>
-        [[nodiscard]] T unwarp_or_else(F const& func) const {
+        template<typename F>
+            requires std::invocable<F>
+        [[nodiscard]] constexpr T unwarp_or_else(F const& func) const {
             return ok() ? unwrap() : func();
         }
 
-        [[nodiscard]] T unwrap_or_default() const requires(std::default_initializable<T>) {
+        [[nodiscard]] constexpr T unwrap_or_default() const
+            requires(std::default_initializable<T>)
+        {
             return ok() ? unwrap() : T();
         }
     };
